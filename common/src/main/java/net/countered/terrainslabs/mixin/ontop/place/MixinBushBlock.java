292a861e8174e1d2a097e8c67db6fb1434c6d1e2
Ada Aster <an.ada.poirier@gmail.com>
2026-05-09 10:04:10 -0400

    Rework bush block mixin to be broadly applicable

--- common/src/main/java/net/countered/terrainslabs/mixin/ontop/place/MixinBushBlock.java
+++ common/src/main/java/net/countered/terrainslabs/mixin/ontop/place/MixinBushBlock.java
@@ -1,30 +1,55 @@
 package net.countered.terrainslabs.mixin.ontop.place;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.countered.platform.PlatformConfigHooks;
-import net.countered.terrainslabs.block.ModBlockTags;
+import net.countered.terrainslabs.block.interfaces.ISlabCopy;
+import net.countered.terrainslabs.util.MixinHelper;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.BushBlock;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(BushBlock.class)
+@Mixin( targets = {
+        "net.minecraft.world.level.block.BushBlock"
+})
 public class MixinBushBlock {
+
+    // TODO: Add tag for can be placed on waterlogged
+    @WrapOperation( method = "canSurvive", at = @At( value = "INVOKE",
+            target = "Lnet/minecraft/world/level/LevelReader;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;"
+    ) )
+    private BlockState terrain_slabs$convertBlockState(
+            LevelReader instance, BlockPos offPos, Operation<BlockState> original,
+            BlockState state, LevelReader level, BlockPos pos
+    ) {
+        BlockState stateAtOffset = original.call( instance, offPos );
+        if (
+                !PlatformConfigHooks.isVegetationOnSlabsEnabled()
+                || !MixinHelper.terrain_slabs$isStateValidOnTop( state )
+                || !( offPos.getX() == pos.getX() && offPos.getZ() == pos.getZ() && offPos.getY() == pos.getY() - 1 )
+        ) {
+            return stateAtOffset;
+        }
+
+        if ( stateAtOffset.getBlock() instanceof ISlabCopy ) {
+            return ISlabCopy.getOriginState( stateAtOffset );
+        }
+
+        return stateAtOffset;
+    }
 
     /**
      * Allows placement of most vegetation on slabs for player and worldgen
      */
-    @Inject(method = "mayPlaceOn", at = @At("HEAD"), cancellable = true)
-    private void terrain_slabs$mayPlaceOn(BlockState state, BlockGetter level, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
-        if (state.is(ModBlockTags.DIRT_SLABS) &&
-                !state.getValue(BlockStateProperties.WATERLOGGED) &&
-                PlatformConfigHooks.isVegetationOnSlabsEnabled())
-        {
-            cir.setReturnValue(true);
-        }
-    }
+//    @Inject(method = "mayPlaceOn", at = @At("HEAD"), cancellable = true)
+//    private void terrain_slabs$mayPlaceOn(BlockState state, BlockGetter level, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+//        if (state.is(ModBlockTags.DIRT_SLABS) &&
+//                !state.getValue(BlockStateProperties.WATERLOGGED) &&
+//                PlatformConfigHooks.isVegetationOnSlabsEnabled())
+//        {
+//            cir.setReturnValue(true);
+//        }
+//    }
 }
