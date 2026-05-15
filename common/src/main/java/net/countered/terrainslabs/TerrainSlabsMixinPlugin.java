05c98794a96ccf9a4d9c75cd8fed2b250f9a3ed8
Ada Aster <an.ada.poirier@gmail.com>
2026-05-15 07:21:26 -0400

    Cancel mixins based on config (borked for fabric)

---
+++ common/src/main/java/net/countered/terrainslabs/TerrainSlabsMixinPlugin.java
@@ -0,0 +1,44 @@
+package net.countered.terrainslabs;
+
+import net.countered.platform.PlatformConfigHooks;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+@SuppressWarnings("unused")
+public final class TerrainSlabsMixinPlugin implements IMixinConfigPlugin {
+
+    private static final List<String> ONTOP_VEGETATION_MIXIN_CLASSES = List.of(
+            "net.countered.terrainslabs.mixin.ontop.place.MixinBlockBehaviours",
+            "net.countered.terrainslabs.mixin.ontop.render.MixinBlockModelShaper",
+            "net.countered.terrainslabs.mixin.ontop.render.MixinLevelRenderer",
+            "net.countered.terrainslabs.mixin.ontop.state.MixinBlock",
+            "net.countered.terrainslabs.mixin.ontop.state.MixinBlockState",
+            "net.countered.terrainslabs.mixin.ontop.state.MixinBlockStateBase"
+    );
+
+
+    @Override
+    public void onLoad( String s ) {}
+
+    /**
+     * Disables vegetation mixins on load instead of during play.
+     */
+    @Override
+    public boolean shouldApplyMixin( String targetClassName, String mixinClassName ) {
+        if ( !PlatformConfigHooks.isVegetationOnSlabsEnabled() && ONTOP_VEGETATION_MIXIN_CLASSES.contains( mixinClassName ) ) {
+            return false;
+        }
+        return true;
+    }
+
+    public String getRefMapperConfig() {return null;}
+    public void acceptTargets(Set<String> set, Set<String> set1) {}
+    public List<String> getMixins() {return null;}
+    public void preApply(String s, ClassNode classNode, String s1, IMixinInfo iMixinInfo) {}
+    public void postApply(String s, ClassNode classNode, String s1, IMixinInfo iMixinInfo) {}
+}
+
