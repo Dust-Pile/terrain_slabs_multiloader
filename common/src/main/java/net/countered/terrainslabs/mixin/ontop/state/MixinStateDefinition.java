9c30f56249f05b6ec1295d25313096d66d465007
Ada Aster <an.ada.poirier@gmail.com>
2026-05-17 21:36:57 -0400

    Forge Friendly Mixin Impl + Cleanup

--- common/src/main/java/net/countered/terrainslabs/mixin/ontop/state/MixinStateDefinition.java
+++ common/src/main/java/net/countered/terrainslabs/mixin/ontop/state/MixinStateDefinition.java
@@ -1,56 +1,39 @@
 package net.countered.terrainslabs.mixin.ontop.state;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.countered.platform.PlatformConfigHooks;
 import net.countered.terrainslabs.block.OffsetProperty;
-import net.countered.terrainslabs.block.interfaces.IOffsetState;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.StateHolder;
 import net.minecraft.world.level.block.state.properties.Property;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import java.util.Map;
 import java.util.function.Function;
 
 @Mixin( StateDefinition.class )
-public abstract class MixinStateDefinition<O, S> {
+public abstract class MixinStateDefinition {
 
-    @Inject( method = "<init>", at = @At( "HEAD" ) )
-    private static <O, S> void terrain_slabs$addOffsetProperty(
-            Function<O, S> stateValueFunction, Object owner,
-            StateDefinition.Factory<O, S> valueFunction,
-            Map<String, Property<?>> propertiesByName, CallbackInfo ci
+    @ModifyVariable( method = "<init>", at = @At("HEAD"), argsOnly = true )
+    private static <O, S> Map<String, Property<?>> terrain_slabs$addOffsetProperty(
+            Map<String, Property<?>> propertiesByName,
+            Function<O, S> stateValueFunction, O owner, StateDefinition.Factory<O, S> valueFunction
     ) {
         if ( !(owner instanceof Block ownerBlock) || !terrain_slabs$shouldAddOffsetStates( ownerBlock ) ) {
-            return;
+            return propertiesByName;
         }
 
         propertiesByName.put( "offset", OffsetProperty.ONTOP);
+        return propertiesByName;
     }
 
-//    /**
-//     * correctly populate states to fix crash on offset state change
-//     */
-//    @WrapOperation( method = "<init>", at = @At( value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/StateHolder;populateNeighbours(Ljava/util/Map;)V" ) )
-//    private void terrain_slabs$populateMirrorNeighbors(
-//            StateHolder<O, S> instance, Map<Map<Property<?>, Comparable<?>>, S> possibleStateMap,
-//            Operation<Void> original
-//    ) {
-//        original.call( instance, possibleStateMap);
-//        if ( instance instanceof IOffsetState offsetState && offsetState.terrain_slabs$hasOffsetState() ) {
-//            original.call( offsetState.terrain_slabs$getOppositeState(), possibleStateMap);
-//        }
-//    }
 
     //==================//
     // Helper Functions //
     //==================//
+
 
     @Unique
     private static boolean terrain_slabs$shouldAddOffsetStates( Block block ) {
