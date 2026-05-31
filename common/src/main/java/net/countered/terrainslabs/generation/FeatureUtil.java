a1f4db3052e64836b5c2e456945214f10fcce9a6
Ada Aster <an.ada.poirier@gmail.com>
2026-05-31 10:57:05 -0400

    Diversify and expand utilities, attempt to catch missed offsets

--- common/src/main/java/net/countered/terrainslabs/generation/FeatureUtil.java
+++ common/src/main/java/net/countered/terrainslabs/generation/FeatureUtil.java
@@ -5,22 +5,20 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.ChunkStatus;
 import net.minecraft.world.level.levelgen.Heightmap;
+import oshi.util.tuples.Pair;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Stack;
+import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
 
 public final class FeatureUtil {
 
-    public static void iterateChunkBlocks( WorldGenLevel level, BlockPos origin, BiConsumer<BlockPos, Integer> handler ) {
+    public static <L extends LevelAccessor> void forEachSurfaceBlock( L level, BlockPos origin, Heightmap.Types heightType, BiConsumer<BlockPos, Integer> handler ) {
         ChunkPos chunkPos = new ChunkPos(origin);
         level.getChunk( chunkPos.x, chunkPos.z );
 
@@ -29,77 +27,166 @@ public final class FeatureUtil {
             for (int z = 0; z < 16; z++) {
                 int worldX = chunkPos.getMinBlockX() + x;
                 int worldZ = chunkPos.getMinBlockZ() + z;
-                int maxY = level.getHeight(Heightmap.Types.WORLD_SURFACE_WG, worldX, worldZ);
-                for (int y = maxY; y >= minY; y--) {
-                    BlockPos currentPos = new BlockPos(worldX, y, worldZ);
-                    handler.accept( currentPos, minY );
-                }
+                int maxY = level.getHeight(heightType, worldX, worldZ);
+                BlockPos topPos = new BlockPos( worldX, maxY, worldZ );
+                handler.accept( topPos, minY );
             }
         }
     }
 
-    public static void iterateInDir(WorldGenLevel level, BlockPos pos, Direction dir, int yLimit, BiFunction<BlockPos, BlockState, Boolean> func) {
+    public static <L extends LevelAccessor> void forEachChunkBlock( L level, BlockPos origin, Heightmap.Types heightType, BiConsumer<BlockPos, Integer> handler ) {
+        forEachSurfaceBlock( level, origin, heightType, (topPos, minY ) -> {
+            int maxY = topPos.getY();
+            for (int y = maxY; y >= minY; y--) {
+                BlockPos currentPos = new BlockPos( topPos.getX(), y, topPos.getZ() );
+                handler.accept( currentPos, maxY );
+            }
+        } );
+    }
+
+    public static <L extends LevelAccessor> boolean iterateDirUntilFail( L level, BlockPos pos, Direction dir, BiFunction<BlockPos, BlockState, Boolean> func) {
+        BlockPos initPos = pos;
+        int yLimit = dir == Direction.UP ? level.getMaxBuildHeight() + 1 : level.getMinBuildHeight() - 1;
+
         while ( pos.getY() != yLimit && func.apply( pos, level.getBlockState( pos ) ) ) {
             pos = pos.relative( dir );
         }
+
+        return pos != initPos;
     }
 
     public static class BlockPosCache {
 
         // Should not be a memory leak... should not be...
         final private static int INITIAL_WARNING_SIZE = 100;
+        final private double GROWTH_FACTOR = 1.5;
         private int warningSize = INITIAL_WARNING_SIZE;
-        final protected Map<ChunkAccess, Stack<BlockPos>> PLACED_SLABS = new HashMap<>();
+        final protected Map<ChunkAccess, Pair<Queue<BlockPos>, LevelAccessor>> CACHE = new HashMap<>( INITIAL_WARNING_SIZE );
+        final BiConsumer<BlockPosCache, ChunkAccess> exitTask;
 
-        public BlockPosCache () {}
+        public BlockPosCache () {
+            this.exitTask = ( cache, chunk ) -> {};
+        }
 
-        public <L extends LevelAccessor> void addSlabPos(L level, BlockPos pos ) {
+        public BlockPosCache ( BiConsumer<BlockPosCache, ChunkAccess> exitTask ) {
+            this.exitTask = exitTask;
+        }
+
+        public boolean containsChunk( ChunkAccess chunk ) {
+            return CACHE.containsKey( chunk );
+        }
+
+        public <L extends LevelAccessor> void addBlockPos(L level, BlockPos pos ) {
             ChunkAccess chunk = level.getChunk( pos );
 
-            if ( PLACED_SLABS.containsKey( chunk ) ) {
-                PLACED_SLABS.get( chunk ).push( pos );
-            } else {
-                mapChunk( chunk );
-                PLACED_SLABS.get( chunk ).push( pos );
+            if ( containsChunk( chunk ) ) {
+                CACHE.get( chunk ).getA().offer( pos );
+            } else if ( mapChunk( level, chunk ) ) {
+                CACHE.get( chunk ).getA().offer( pos );
             }
         }
 
-        protected Optional<Stack<BlockPos>> getChunk(ChunkAccess chunk ) {
-            if ( PLACED_SLABS.containsKey( chunk ) ) {
-                return Optional.of( PLACED_SLABS.get( chunk ) );
-            }
-
-            return Optional.empty();
-        }
-
-        protected Optional<Stack<BlockPos>> popChunk( ChunkAccess chunk ) {
-            Optional<Stack<BlockPos>> stackOption = getChunk( chunk );
-            PLACED_SLABS.remove( chunk );
-
-            return stackOption;
-        }
-
-        private void mapChunk( ChunkAccess chunk ) {
-            if ( PLACED_SLABS.containsKey( chunk ) || chunk.getStatus().isOrAfter( ChunkStatus.INITIALIZE_LIGHT ) ) {
+        /**
+         * Explicitly allows adding more items to queue. Will only run for item at time of start.
+         */
+        protected void forExistingQueue( ChunkAccess chunk, Consumer<BlockPos> handler ) {
+            if( !containsChunk( chunk ) ) {
                 return;
             }
 
-            if ( PLACED_SLABS.size() >= warningSize) {
+            Queue<BlockPos> queue = this.getQueue( chunk );
+            assert queue != null;
+            for (int i = queue.size(); i > 0; i-- ) {
+                handler.accept( queue.poll() );
+            }
+        }
+
+        private Queue<BlockPos> getQueue( ChunkAccess chunk ) {
+            if ( containsChunk( chunk ) ) {
+                return CACHE.get( chunk ).getA();
+            }
+
+            return null;
+        }
+
+        protected LevelAccessor getLevel( ChunkAccess chunk ) {
+            if ( containsChunk( chunk ) ) {
+                return CACHE.get( chunk ).getB();
+            }
+
+            return null;
+        }
+
+        protected boolean mapChunk( LevelAccessor level, ChunkAccess chunk ) {
+            if ( containsChunk( chunk ) || chunk.getStatus().isOrAfter( ChunkStatus.INITIALIZE_LIGHT ) ) {
+                return false;
+            }
+
+            if ( CACHE.size() >= warningSize) {
                 trimCache();
             }
 
-            PLACED_SLABS.put( chunk, new Stack<>() );
+            CACHE.put( chunk, new Pair<>( new LinkedList<>(), level ) );
+            return true;
         }
 
         private void trimCache() {
-            PLACED_SLABS.forEach( ( chunk, stack ) -> {
-                if ( chunk.getStatus().isOrAfter( ChunkStatus.INITIALIZE_LIGHT ) ) {
-                    PLACED_SLABS.remove( chunk );
-                }
+            List<ChunkAccess> chunksToRemove = new ArrayList<>();
+            CACHE.forEach( (chunk, stack ) -> {
+                if ( shouldTrimChunk( chunk ) ) chunksToRemove.add( chunk );
             } );
 
-            TerrainSlabs.LOGGER.warn( "Placed Slab Cache grew to size {}; trimmed to size {}.", warningSize, PLACED_SLABS.size() );
-            warningSize = Math.max( PLACED_SLABS.size() * 2, INITIAL_WARNING_SIZE );
+            chunksToRemove.forEach( this::removeChunk );
+            TerrainSlabs.LOGGER.warn( "Placed Slab Cache grew to size {}; trimmed to size {}.", warningSize, CACHE.size() );
+            warningSize = Math.max( (int) ( CACHE.size() * GROWTH_FACTOR ), INITIAL_WARNING_SIZE );
+        }
+
+        protected void tryTrimChunks( ChunkAccess origin ) {
+            Collection<ChunkAccess> neighbors = this.getNeighbors( origin );
+            if ( shouldTrimChunk( origin ) ) {
+                tryTrimChunk( origin );
+                neighbors.forEach( this::tryTrimChunks );
+            }
+        }
+
+        protected void tryTrimChunk( ChunkAccess chunk ) {
+            if ( shouldTrimChunk( chunk ) ) {
+                removeChunk( chunk );
+            }
+        }
+
+        private void removeChunk( ChunkAccess chunk ) {
+            exitTask.accept( this, chunk );
+            CACHE.remove( chunk );
+        }
+
+        private boolean shouldTrimChunk(ChunkAccess chunk ) {
+            if ( !containsChunk( chunk ) ) {
+                return false;
+            }
+
+            return chunk.getStatus().isOrAfter( ChunkStatus.SPAWN );
+        }
+
+        protected Collection<ChunkAccess> getNeighbors( ChunkAccess chunk ) {
+            if ( !containsChunk( chunk ) ) {
+                return List.of();
+            }
+
+            Collection<ChunkAccess> output = new ArrayList<>();
+            LevelAccessor level = this.getLevel( chunk );
+            int centerX = chunk.getPos().x;
+            int centerZ = chunk.getPos().z;
+
+            int size = 1;
+            for ( int x = size; x >= -size; x-- ) {
+                for ( int z = size; z >= -size; z-- ) {
+                    if ( x == 0 && z == 0 ) continue;
+                    output.add( level.getChunk( centerX + x, centerZ + z ) );
+                }
+            }
+
+            return output;
         }
     }
 }
--- common/src/main/java/net/countered/terrainslabs/generation/OffsetFeature.java
+++ common/src/main/java/net/countered/terrainslabs/generation/OffsetFeature.java
@@ -5,20 +5,24 @@ import net.countered.platform.PlatformConfigHooks;
 import net.countered.terrainslabs.block.interfaces.IOffsetState;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SlabBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.SlabType;
 import net.minecraft.world.level.chunk.ChunkAccess;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
 
-import java.util.Optional;
-import java.util.Stack;
-
 public class OffsetFeature extends Feature<NoneFeatureConfiguration> {
+
+    protected static final FeatureUtil.BlockPosCache BOTTOM_SLAB_CACHE = new FeatureUtil.BlockPosCache(
+            ( cache, chunk ) -> {
+                fixChunkOffsetsCached( cache.getLevel( chunk ), chunk.getPos().getWorldPosition(), cache, Direction.UP );
+            } );
 
     public OffsetFeature(Codec<NoneFeatureConfiguration> codec) {
         super(codec);
@@ -32,53 +36,72 @@ public class OffsetFeature extends Feature<NoneFeatureConfiguration> {
             return false;
         }
 
-        ChunkAccess chunk = context.level().getChunk( context.origin() );
-        Optional<Stack<BlockPos>> topSlabs = SlabFeature.TOP_SLAB_CACHE.popChunk( chunk );
-        if ( topSlabs.isPresent() ) {
-            fixAllOffsetsCached( topSlabs.get(), Direction.UP, context );
+        LevelAccessor level = context.level();
+        BlockPos origin = context.origin();
+        ChunkAccess chunk = level.getChunk( origin );
+        if ( BOTTOM_SLAB_CACHE.containsChunk( chunk ) ) {
+            fixChunkOffsetsCached( level, origin, BOTTOM_SLAB_CACHE, Direction.UP );
         } else {
-            fixAllOffsets( context );
+            fixChunkOffsets( context );
         }
+        fixSurfaceOffsets( Heightmap.Types.MOTION_BLOCKING, context );
+
+        BOTTOM_SLAB_CACHE.getNeighbors( chunk ).forEach( BOTTOM_SLAB_CACHE::tryTrimChunks );
         return true;
     }
 
-    private void fixAllOffsetsCached( Stack<BlockPos> stack, Direction dir, FeaturePlaceContext<NoneFeatureConfiguration> context ) {
+    private static void fixChunkOffsetsCached( LevelAccessor level, BlockPos origin, FeatureUtil.BlockPosCache cache, Direction dir) {
         if ( !dir.getAxis().isVertical() ) {
             throw new IllegalArgumentException(
-                    "Direction inputl to 'fixAllOffsetsCached' in 'OffsetFeature' must have vertical axis"
+                    "Direction input to 'fixAllOffsetsCached' in 'OffsetFeature' must have vertical axis"
             );
         }
 
-        WorldGenLevel level = context.level();
-        int heightLimit = dir == Direction.UP ? level.getMaxBuildHeight() + 1 : level.getMinBuildHeight() - 1;
-
-        while ( !stack.isEmpty() ) {
-            BlockPos pos = stack.pop();
-            BlockState state = context.level().getBlockState( pos );
-
-            if ( ( state.getBlock() instanceof SlabBlock ) ) {
-                FeatureUtil.iterateInDir( context.level(), pos.relative( dir ), dir, heightLimit,
-                        ( aPos, aState ) -> placeOnTopState( level, aPos, aState )
-                );
-            }
-        }
-    }
-
-    // This is a fallback method (not preferred)
-    private void fixAllOffsets( FeaturePlaceContext<NoneFeatureConfiguration> context ) {
-        WorldGenLevel level = context.level();
-        FeatureUtil.iterateChunkBlocks( level, context.origin(), ( pos, maxY ) -> {
-            BlockState state = level.getBlockState( pos );
-
-            if ( ( state.getBlock() instanceof SlabBlock && state.getValue( SlabBlock.TYPE ) == SlabType.BOTTOM ) ) {
-                FeatureUtil.iterateInDir( context.level(), pos.above(), Direction.UP, level.getMaxBuildHeight(),
-                        ( aPos, aState ) -> placeOnTopState( level, aPos, aState )
-                );
+        ChunkAccess chunk = level.getChunk( origin );
+        cache.forExistingQueue( chunk, ( pos ) -> {
+            if ( !replaceStatesInDir( level, pos, dir ) ) {
+                cache.addBlockPos( level, pos );
             }
         } );
     }
 
-    private boolean placeOnTopState(WorldGenLevel level, BlockPos pos, BlockState state ) {
+    // This is a fallback method (not preferred)
+    private static void fixChunkOffsets(FeaturePlaceContext<NoneFeatureConfiguration> context ) {
+        WorldGenLevel level = context.level();
+        FeatureUtil.forEachChunkBlock( level, context.origin(), Heightmap.Types.WORLD_SURFACE_WG, (pos, maxY ) -> {
+            BlockState state = level.getBlockState( pos );
+
+            if ( !( state.getBlock() instanceof SlabBlock ) ) {
+                return; //continue
+            }
+
+            if ( ( state.getValue( SlabBlock.TYPE ) == SlabType.BOTTOM )
+                    && !replaceStatesInDir( level, pos.above(), Direction.UP )
+            ) {
+                // We're here, may as well populate the cache.
+                BOTTOM_SLAB_CACHE.addBlockPos( level, pos );
+            }
+        } );
+
+        // Ensure cache for chunk exists
+        BOTTOM_SLAB_CACHE.mapChunk( level, level.getChunk( context.origin() ) );
+    }
+
+    private static void fixSurfaceOffsets( Heightmap.Types heightType, FeaturePlaceContext<NoneFeatureConfiguration> context ) {
+        WorldGenLevel level = context.level();
+        FeatureUtil.forEachSurfaceBlock( level, context.origin(), heightType, (topPos, minY ) -> {
+            replaceStatesInDir( level, topPos.above(), Direction.UP );
+        } );
+    }
+
+    private static boolean replaceStatesInDir( LevelAccessor level, BlockPos pos, Direction dir ) {
+        return FeatureUtil.iterateDirUntilFail( level, pos.relative( dir ), dir,
+                ( aPos, aState ) -> placeOntopState( level, aPos, aState )
+        );
+    }
+
+    // TODO: Rename and handle onbottom states
+    private static boolean placeOntopState( LevelAccessor level, BlockPos pos, BlockState state ) {
         return IOffsetState.shouldBeOnTopState( level, pos, state )
                 && level.setBlock( pos, ((IOffsetState) state ).terrain_slabs$getOffsetState(), Block.UPDATE_CLIENTS );
     }
